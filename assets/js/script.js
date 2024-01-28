8fb3a275d25bfa11136ab9e3b04fae96e69549ed
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-01-28 11:59:21 +0000

    Create js folder and script fille to add menu toggle functionality for mobile devices

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// This is the javascript file to hide and show the menu on mobile devices
+// Get the checkbox input and the nav element
+var menuToggle = document.getElementById("menu-toggle");
+var nav = document.getElementById("sidebar-menu");
+
+
+// Get all the links inside the nav element
+var links = nav.getElementsByTagName("a");
+
+// Loop through the links and add a click event listener
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+  
+// When a link is clicked, close the nav element
+menuToggle.checked = false;
+  });
+}
